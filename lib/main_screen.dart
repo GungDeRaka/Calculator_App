43f9b37306f06bb86ef63958960496f94af72a6c
GungDeRaka <aswinraka4@gmail.com>
2023-01-09 12:50:55 +0800

    tryna add calculator logic

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_calculator_app/my_button.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -9,6 +8,10 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  String result = "0";
+  String number = "";
+  String operator = "";
+  String btnText = "";
   List<String> btnList = [
     'C',
     '+/-',
@@ -30,48 +33,147 @@ class _MainScreenState extends State<MainScreen> {
     '0',
     "⌫",
     '=',
-
   ];
+
+
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 12.0,
+            ),
+            Container(
+              width: size.width * 0.95,
+              height: size.height * 0.3,
+              margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 12,
+                      blurStyle: BlurStyle.normal)
+                ],
+                borderRadius: BorderRadius.circular(12),
                 color: Colors.white,
-                width: size.width * 0.8,
-                height: size.height * 0.3,
-                padding: const EdgeInsets.all(32),
               ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
-                    childAspectRatio: 1.15
-                  ),
-                  itemCount: btnList.length,
-                  shrinkWrap: true,
-                  physics: const ScrollPhysics(),
-                  itemBuilder: (BuildContext context, int index) {
-                    return MyButton(btnText: btnList[index]);
-                  },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      number,
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(
+                      result,
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  childAspectRatio: 1.4,
+                  mainAxisExtent: 60,
+                ),
+                itemCount: btnList.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  btnText = btnList[index];
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: (btnText == "=")
+                          ? Colors.red[700]
+                          : (btnText == "+" ||
+                                  btnText == "-" ||
+                                  btnText == "x" ||
+                                  btnText == "÷")
+                              ? Colors.amber[700]
+                              : (btnText == "C" ||
+                                      btnText == "+/-" ||
+                                      btnText == "%" ||
+                                      btnText == "." ||
+                                      btnText == "⌫")
+                                  ? Colors.blueGrey[300]
+                                  : Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12), // <-- Radius
+                      ),
+                    ),
+                    onPressed: () {
+                      (btnText != "C" ||btnText != "⌫" ||btnText != "+/-" ||btnText != "=")?
+                      number = number.padRight(number.length+1,btnList[index]):number = number;
+                      setState(() {
+                        
+                      });
+                    },
+                    child: Text(
+                      btnText,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+          ],
         ),
       ),
     );
+  }
+    void count(){
+    num firstNum = num.parse(number.substring(0, number.indexOf(operator)));
+    num secondNum = num.parse(number.substring(number.indexOf(operator)+1));
+    if(btnText == "+"){
+      operator= "+";
+      result = (firstNum + secondNum).toString();
+    }
+    else if(btnText == "-"){
+      operator= "-";
+      result = (firstNum - secondNum).toString();
+    }
+    else if(btnText == "x"){
+      operator= "x";
+      result = (firstNum * secondNum).toString();
+    }
+    else if(btnText == "÷"){
+      operator= "÷";
+      result = (firstNum / secondNum).toString();
+    }
+
   }
 }
